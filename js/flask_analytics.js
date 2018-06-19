f6a40be3594204de63d2b320ae2c85593a9fc877
ogawahideko <ogawahideko@gmail.com>
2018-06-19 17:15:51 +0900

    fixed wrong link

--- js/flask_analytics.js
+++ js/flask_analytics.js
@@ -1,4 +1,4 @@
-//Auto Link Maker
+//Auto Link Maker by iTunes
 var _merchantSettings=_merchantSettings || [];_merchantSettings.push(['AT', '1l3vkXJ']);(function(){var autolink=document.createElement('script');autolink.type='text/javascript';autolink.async=true; autolink.src= ('https:' == document.location.protocol) ? 'https://autolinkmaker.itunes.apple.com/js/itunes_autolinkmaker.js' : 'http://autolinkmaker.itunes.apple.com/js/itunes_autolinkmaker.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(autolink, s);})();
 
 // Google Analytics
